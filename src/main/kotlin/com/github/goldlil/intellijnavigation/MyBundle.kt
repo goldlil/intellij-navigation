0be057e65bb6aef8fd0b7023e17dfe793113ab24
GitHub Action <action@github.com>
2021-08-20 17:55:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/goldlil/intellijnavigation/MyBundle.kt
+++ src/main/kotlin/com/github/goldlil/intellijnavigation/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.goldlil.intellijnavigation
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/goldlil/intellijnavigation/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.goldlil.intellijnavigation.services
+
+import com.github.goldlil.intellijnavigation.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
